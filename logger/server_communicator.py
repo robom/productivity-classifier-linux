c6de0de8af0406bf0277163c67a785be7f3d3625
Robert Moro <robo.moro@gmail.com>
2016-04-20 21:48:48 +0200

    Add timestamp to JSON

--- logger/server_communicator.py
+++ logger/server_communicator.py
@@ -7,11 +7,13 @@ class ServerCommunicator(object):
         logging.debug(message)
         logging.debug(params)
 
+        timestamp = datetime.now().isoformat(' ')
+
         try:
             request = requests.post(url=Config.load_url(), data={
                 "Token": Config.load_token(),
-                "Value": str({'message': message, 'params': params}),
-                "ValidFrom": datetime.now().isoformat(' ')
+                "Value": str({'message': message, 'params': params, 'timestamp': timestamp}),
+                "ValidFrom": timestamp
             })
 
             if request.status_code >= 400:
